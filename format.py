8003e1ff07c30b4e2440bf628c4e85e40d0c2f0b
tvieregge <tmvieregge@gmail.com>
2018-11-23 15:50:50 -0500

    removing entries with missing data

--- format.py
+++ format.py
@@ -5,6 +5,8 @@ data_start = 25
 directory = "./data"
 
 with open("output", 'w') as output_file:
+    removed_count = 0
+    total_count = 0
     output_writer = csv.writer(output_file)
     for filename in os.listdir(directory):
         if not filename.endswith(".csv"):
@@ -18,4 +20,11 @@ with open("output", 'w') as output_file:
             for row in reader:
                 #print(row)
                 data = [row[i] for i in [1,2,3,5,7]]
-                output_writer.writerow(data)
+                if data.count('') == 0:
+                    output_writer.writerow(data)
+                else:
+                    removed_count += 1
+                total_count += 1
+
+    print("removed {}/{} entries because of missing data".format(removed_count, total_count))
+
