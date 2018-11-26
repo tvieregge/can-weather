c862e473c3d97b868cdf0868e94b49aecf4df9a9
tvieregge <tmvieregge@gmail.com>
2018-11-25 20:30:49 -0500

    generates averages by year of single month

--- analyze.py
+++ analyze.py
@@ -1,15 +1,24 @@
 import csv
+from collections import defaultdict
+
+def filter_data(data, month):
+    return [(int(entry[0]), float(entry[3]))
+            for entry in filter(lambda datum: int(datum[1])==month, data)]
+
+def group(l):
+    by_year = defaultdict(list)
+    for entry in l:
+        by_year[entry[0]].append(entry[1])
+    return by_year
+
+def avg(data):
+    return [(year, sum(data[year])/len(data[year]))
+            for year in data.keys()]
 
 with open("output") as data_file:
-    data = csv.reader(data_file)
-    months = list(range(1,13))
-    print(months)
+    data = list(csv.reader(data_file))
+    print(
+    list(
+    avg(
+    group(filter_data(data, 1)))))
 
-    def filter_data(month):
-        print(month)
-        return list(filter(lambda datum: int(datum[1])==month, data))
-
-    print(filter_data(3))
-    # print(
-    #     list(
-    #         map(filter_data, months)))
