90baef61849d06155e63a372221d9fa409f90be9
tvieregge <tmvieregge@gmail.com>
2018-11-26 17:56:10 -0500

    comments and cleanup

--- analyze.py
+++ analyze.py
@@ -2,28 +2,31 @@ import csv
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
+# Variables for controlling the analysis
+#   data:column refers to the column in the "output" file
+#   aperture refers to the width to use for the running average
+data_column = 1
+aperture = 1
+
 def filter_data(data, month):
     return [(int(entry[0]), float(entry[3]))
             for entry in filter(lambda datum: int(datum[1])==month, data)]
 
-def group_days(l):
+def group_days(column, l):
     by_year = defaultdict(list)
     for entry in l:
-        by_year[entry[0]].append(entry[1])
+        by_year[entry[0]].append(entry[column])
     return by_year
 
-def avg_dict(data):
-    return [(year, sum(data[year])/len(data[year]))
-            for year in data.keys()]
-
-def do_month(data, month):
+def process_month(data, month, data_column):
     return sorted(
             tuple(
-            avg_dict(
-            group_days(
+            avg_years(
+            group_days(data_column,
             filter_data(data, month)))))
 
 def window(l, index, aperture):
+    # don't want results that can't be averaged over the whole window
     if index >= len(l)-half_ap or index < half_ap:
         return []
     left = index-half_ap
@@ -31,6 +34,10 @@ def window(l, index, aperture):
     temp = [l[left:right]]
     temp.extend(window(l, index+1, half_ap))
     return temp
+
+def avg_years(data):
+    return [(year, avg(data[year]))
+            for year in data.keys()]
 
 def avg(l):
     return sum(l)/(len(l) or 1)
@@ -46,11 +53,9 @@ def clip_x_vals(x_vals, half_ap):
 with open("output") as data_file:
     months = tuple(range(1,13))
     data = tuple(csv.reader(data_file))
-    results = [do_month(data, month) for month in months]
+    results = [process_month(data, month, data_column) for month in months]
 
-    aperture = 5
     half_ap = aperture//2
-
     for r in results:
         x_val = [datum[0] for datum in r]
         y_val = [datum[1] for datum in r]
